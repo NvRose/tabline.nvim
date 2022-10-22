b3e631cd954e4e4f32855e63eb16b3f65351e040
fenze <contact@fenze.dev>
2022-10-22 22:32:41 +0200

    Inject tab.lua into init.lua

--- lua/tabline/init.lua
+++ lua/tabline/init.lua
@@ -1,3 +1,4 @@
+local fn = vim.fn
 local cfg = {}
 
 local default = {
@@ -10,6 +11,55 @@ local default = {
 		{ filetype = 'TelescopePrompt', extension = 'telescope' }
 	}
 }
+
+local title = function(bufnr, custom)
+	local f = fn.bufname(bufnr)
+	local bt = fn.getbufvar(bufnr, '&buftype')
+	local ft = fn.getbufvar(bufnr, '&filetype')
+
+	for _, i in ipairs(custom) do
+		if i.filetype == ft then
+			return i.title
+		end
+	end
+
+	return fn.pathshorten(fn.fnamemodify(f, ':p:~:t'))
+end
+
+local devicon = function(bufname, bufnr, custom)
+	local extension = fn.expand('#'..bufnr..':e')
+	local devicon = require('nvim-web-devicons').get_icon
+
+	for _, i in ipairs(custom) do
+		if i.filetype == extension then
+			local icon, color = devicon(i.extension)
+			if icon then icon = icon .. ' ' end
+			return icon
+		end
+	end
+
+	local icon, color = devicon(bufname, extension)
+	if icon then icon = icon .. ' ' end
+	return icon
+end
+
+local tab = function(index, config)
+	local winnr = fn.tabpagewinnr(index)
+	local bufnr = fn.tabpagebuflist(index)[winnr]
+	local bufname = fn.bufname(bufnr)
+
+	local selected = fn.tabpagenr() == index
+	local modified = vim.fn.getbufvar(bufnr, '&modified') == 1
+
+	return (selected and '%#TabLineSel#' or '%#TabLine#')
+		.. (modified and '%#TabLineModified#' or '')
+		.. string.rep(' ', config.padding)
+		.. (config.devicons
+			and devicon(bufname, bufnr, config.custom_devicons_filetypes)
+			or '')
+		.. title(bufnr, config.custom_titles)
+		.. string.rep(' ', config.padding)
+end
 
 local run = function()
 	local s = ''
