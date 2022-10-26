7b452be81959a6baf1f43ac585ebb31cececa95f
fenze <contact@fenze.dev>
2022-10-27 00:14:41 +0200

    Add fixed width

--- init.lua
+++ init.lua
@@ -2,8 +2,6 @@ local fn = vim.fn
 local cfg = {}
 
 local default = {
-	devicons = true,
-	padding = 3,
 	custom_titles = {
 		{ filetype = "TelescopePrompt", title = "telescope" },
 	},
@@ -12,20 +10,7 @@ local default = {
 	},
 }
 
-local title = function(bufnr, custom)
-	local f = fn.bufname(bufnr)
-	local ft = fn.getbufvar(bufnr, "&filetype")
-
-	for _, i in ipairs(custom) do
-		if i.filetype == ft then
-			return i.title
-		end
-	end
-
-	return vim.fn.fnamemodify(f, ":t")
-end
-
-local devicon = function(bufnr, custom)
+local devicon = function(bufnr)
 	local ft = fn.getbufvar(bufnr, "&filetype")
 	local devicons = require("nvim-web-devicons")
 
@@ -37,26 +22,49 @@ local devicon = function(bufnr, custom)
 	end
 end
 
+local function get_cols()
+	local f = assert(io.popen("tput cols", "r"))
+	local cols = f:read("a")
+	f:close()
+
+	return tonumber(cols)
+end
+
+local get_title = function(bufnr, custom)
+	local f = fn.bufname(bufnr)
+	local ft = fn.getbufvar(bufnr, "&filetype")
+
+	for _, i in ipairs(custom) do
+		if i.filetype == ft then
+			return i.title
+		end
+	end
+
+	local title = devicon(bufnr) .. fn.fnamemodify(f, ":t")
+	local title_len = string.len(title)
+	local win_width = get_cols()
+
+	local padding = math.floor(((win_width / fn.tabpagenr("$") - title_len) + 0.5) / 2) + 1
+
+	return string.rep(" ", padding) .. title .. string.rep(" ", padding)
+end
+
 local tab = function(index, config)
 	local winnr = fn.tabpagewinnr(index)
 	local bufnr = fn.tabpagebuflist(index)[winnr]
-	local bufname = fn.bufname(bufnr)
 
 	local selected = fn.tabpagenr() == index
-	local modified = vim.fn.getbufvar(bufnr, "&modified") == 1
+	local modified = fn.getbufvar(bufnr, "&modified") == 1
 
 	return (selected and "%#TabLineSel#" or "%#TabLine#")
 		.. (modified and "%#TabLineModified#" or "")
-		.. string.rep(" ", config.padding)
-		.. (config.devicons and devicon(bufnr, config.custom_devicons_filetypes) or "")
-		.. title(bufnr, config.custom_titles)
-		.. string.rep(" ", config.padding)
+		.. get_title(bufnr, config.custom_titles)
 end
 
 local run = function()
 	local s = ""
 
-	for i = 1, vim.fn.tabpagenr("$"), 1 do
+	for i = 1, fn.tabpagenr("$"), 1 do
 		s = s .. "%" .. i .. "T" .. tab(i, cfg)
 	end
 
@@ -69,7 +77,7 @@ return {
 	run = run,
 	setup = function(config)
 		cfg = config and vim.tbl_deep_extend("force", default, config) or default
-
 		vim.opt.tabline = '%!v:lua.require("NvRose.base.tabline").run()'
+		vim.cmd("redrawtabline")
 	end,
 }
